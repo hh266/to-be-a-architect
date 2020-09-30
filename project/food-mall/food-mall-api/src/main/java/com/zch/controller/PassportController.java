49b5d7121f488874e19044fbcfb0dccf2ab526e5
zhang0319 <zhang13685512820>
2020-09-30 17:11:47 +0800

    引入 Hutool 工具

--- project/food-mall/food-mall-api/src/main/java/com/zch/controller/PassportController.java
+++ project/food-mall/food-mall-api/src/main/java/com/zch/controller/PassportController.java
@@ -1,10 +1,9 @@
 package com.zch.controller;
 
+import cn.hutool.core.util.StrUtil;
 import com.zch.result.CommonResult;
 import com.zch.service.UserService;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +21,7 @@ public class PassportController {
     @GetMapping("/usernameIsExist")
     public CommonResult usernameIsExist(String username) {
         // 1.判断用户名不能为空
-        if (StringUtils.isBlank(username)) {
+        if (StrUtil.isBlank(username)) {
             // 返回参数验证失败的结果
             return CommonResult.validateFailed();
         }
