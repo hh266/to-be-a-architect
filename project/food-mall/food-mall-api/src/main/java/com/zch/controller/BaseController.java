1df4adee730355f059647d278f920678bdf86cfe
zhang0319 <zhang13685512820>
2020-10-15 16:52:18 +0800

    baseController

--- project/food-mall/food-mall-api/src/main/java/com/zch/controller/BaseController.java
+++ project/food-mall/food-mall-api/src/main/java/com/zch/controller/BaseController.java
@@ -1,12 +1,9 @@
 package com.zch.controller;
 
-import org.springframework.stereotype.Controller;
-
 /**
  * @author zch
  * @date 2020/10/15 15:41
  */
-@Controller
 public class BaseController {
 
     public static final Integer COMMON_PAGE_SIZE = 10;
