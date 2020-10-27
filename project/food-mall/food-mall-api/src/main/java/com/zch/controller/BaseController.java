6dd9a8b5d10cc72359b39e44cd1745b38480cf6c
hh266 <1297293405@qq.com>
2020-10-27 23:57:28 +0800

    支付3

--- project/food-mall/food-mall-api/src/main/java/com/zch/controller/BaseController.java
+++ project/food-mall/food-mall-api/src/main/java/com/zch/controller/BaseController.java
@@ -12,6 +12,18 @@ public class BaseController {
     public static final Integer ITEM_PAGE_SIZE = 20;
 
     /**
+     * imooc 支付中心账号密码
+     */
+    String imoocUserId = "tmp2020";
+    String imoocPassword = "tmp2020";
+
+    /**
+     * 支付中心的调用地址
+     */
+    String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";
+
+
+    /**
      * 微信支付成功 -> 支付中心 -> 天天吃货平台
      * 回调中心的url
      */
--- project/food-mall/food-mall-api/src/main/java/com/zch/controller/OrdersController.java
+++ project/food-mall/food-mall-api/src/main/java/com/zch/controller/OrdersController.java
@@ -11,12 +11,13 @@ import com.zch.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,6 +35,9 @@ public class OrdersController extends BaseController {
     @Autowired
     private OrderService service;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @ApiOperation(value = "添加订单", notes = "添加订单", httpMethod = "POST")
     @PostMapping("/create")
     public CommonResult create(@Validated @RequestBody SubmitOrderBO submitOrderBO,
@@ -42,17 +46,27 @@ public class OrdersController extends BaseController {
         // 1. 创建购物车
         OrderVO orderVO = service.create(submitOrderBO);
         String orderId = orderVO.getOrderId();
-        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
+
 
         // 2. 移除购物车中的订单
         // TODO 移除 redis 中购物车的数据，并且同步到 前端 Cookie
         // 移除cookie中的数据
         // CookieUtils.setCookie(request, response, FOODIE_SHOPCART,  "", true);
 
-
         // 3.向购物中心发送当前订单，用于保存支付中心的订单数据
+        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
         merchantOrdersVO.setReturnUrl(payReturnUrl);
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("imoocUserId",imoocUserId);
+        headers.add("password",imoocPassword);
+
+        HttpEntity<MerchantOrdersVO> entity = new HttpEntity<>(merchantOrdersVO, headers);
+
+       ResponseEntity<CommonResult> responseEntity = restTemplate.postForEntity(paymentUrl, entity, CommonResult.class);
+
+        responseEntity.getBody();
 
         return CommonResult.success(orderId,"ok");
     }
