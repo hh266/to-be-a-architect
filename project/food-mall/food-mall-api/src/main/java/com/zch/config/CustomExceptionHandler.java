19f4467bf54be90afde45d919822d8dc70bc41f5
hh266 <1297293405@qq.com>
2020-11-03 23:30:33 +0800

    设置文件大小,抛出异常返回给前端

---
+++ project/food-mall/food-mall-api/src/main/java/com/zch/config/CustomExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.zch.config;
+
+import com.zch.result.CommonResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    /**
+     * 上传文件超过500k，捕获异常：MaxUploadSizeExceededException
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public CommonResult handlerMaxUploadFile(MaxUploadSizeExceededException ex) {
+        return CommonResult.failed("文件上传大小不能超过500k，请压缩图片或者降低图片质量再上传！");
+    }
+}
