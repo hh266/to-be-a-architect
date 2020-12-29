7c65160be32b3ea63e96f56111e45b9bb48d5877
hh266 <1297293405@qq.com>
2020-12-29 22:21:28 +0800

    实现了 文件上传与删除

--- project/fastdfs-demo/src/main/java/com/zch/fastdfs/config/SwaggerConfig.java
+++ project/fastdfs-demo/src/main/java/com/zch/fastdfs/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Swagger 配置
@@ -16,13 +17,14 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @date 2020/12/29 17:38
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.zch.fasrdfs"))
+                .apis(RequestHandlerSelectors.basePackage("com.zch.fastdfs"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- project/fastdfs-demo/src/main/java/com/zch/fastdfs/controller/FileController.java
+++ project/fastdfs-demo/src/main/java/com/zch/fastdfs/controller/FileController.java
@@ -1,25 +1,42 @@
 package com.zch.fastdfs.controller;
 
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * @author zch
  * @date 2020/12/29 17:44
  */
+@Api(value = "文件管理", tags = {"文件相关的接口"})
 @RestController
 @RequestMapping("/file")
 public class FileController {
 
     @Autowired
-    private FastFileStorageClient fastFileStorageClient;
+    private FastFileStorageClient storageClient;
 
-    @PostMapping()
-    public String uploadFile(MultipartFile File){
+    @PostMapping("/upload")
+    @ApiOperation(value = "文件上传", httpMethod="POST")
+    public String uploadFile(MultipartFile file) throws IOException {
+        StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), file.getOriginalFilename(), null);
+        return  storePath.getFullPath();
+    }
 
+    @PostMapping("/delete")
+    @ApiOperation(value = "文件删除", httpMethod="POST")
+    public String deleteFile(String filePath) {
+        storageClient.deleteFile(filePath);
+        return "OK";
     }
 }
