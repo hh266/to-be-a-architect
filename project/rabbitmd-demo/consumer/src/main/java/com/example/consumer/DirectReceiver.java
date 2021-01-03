3c33dc049987e0699ccffd63ebf5ec347f85c6f8
hh266 <1297293405@qq.com>
2021-01-03 20:55:33 +0800

    Add RabbitMQ demo

---
+++ project/rabbitmd-demo/consumer/src/main/java/com/example/consumer/DirectReceiver.java
@@ -0,0 +1,23 @@
+package com.example.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @description: DirectReceiver
+ * @author: zch
+ * @create: 2021-01-03 19:16
+ */
+
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+}
